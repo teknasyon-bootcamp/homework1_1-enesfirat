4cf76a1d607383584724472735a6d1cfde221580
Enes Firat Dogan <efd@Ibrahim-MacBook-Pro.local>
2021-08-18 16:20:15 +0300

    I have completed my first assignment in php

--- app.php
+++ app.php
@@ -2,6 +2,21 @@
 
 $age = 25;
 
+if ( 14 >= $age && $age >= 0) {
+    echo "Çocuk";
+}
+elseif (24 >= $age && $age >= 15) {
+    echo "Genc";
+}
+elseif (64 >= $age && $age >= 25) {
+    echo "Yetiskin";
+}
+elseif ($age > 64) {
+    echo "Yasli";
+}
+else {
+    echo "Tanimlanamayan bir deger girdiniz!";
+}
 /**
  * Bu ödevde age değişkeni içerisinde yer alan yaş değerinin bulunduğu
  * yaş grubu tespit edilip ekrana bu yaş grubunun ismi yazılmalıdır. 
